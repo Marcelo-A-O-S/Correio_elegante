9d697f0d130f1e9ce616ed2af2c1d541e9699504
Marcelo-A-O-S <marceloaugustooliveirasoares@gmail.com>
2025-06-11 14:40:52 -0300

    Adicionando botão para função de repetição

--- client/src/app/carta/[id]/CartaShow.tsx
+++ client/src/app/carta/[id]/CartaShow.tsx
@@ -12,17 +12,21 @@ interface CartaShowProps {
 export default function CartaShow({ cartaCurrent, initialTrechos }: CartaShowProps) {
     const [currentImage, setCurrentImage] = useState<string | null>(null);
     const [trechos, setTrechos] = useState<ITrecho[]>([]);
+    const [showReloadButton, setShowReloadButton] = useState(false);
     useEffect(() => {
         if (initialTrechos && initialTrechos.length > 0) {
             setTrechos(initialTrechos);
         }
     }, [initialTrechos]);
     const sequence = trechos.length > 0
-    ? trechos.flatMap((trecho) => {
+    ? trechos.flatMap((trecho, index) => {
         const items: (string | number | (() => void))[] = [];
         items.push(() => {
             if (trecho.imageUrl) setCurrentImage(trecho.imageUrl);
         });
+        if (index === trechos.length - 1) {
+            items.push(() => setShowReloadButton(true));
+          }
         items.push(trecho.content);
         items.push(3000);
         return items;
@@ -45,13 +49,21 @@ export default function CartaShow({ cartaCurrent, initialTrechos }: CartaShowPro
                     sequence={sequence}
                     wrapper="span"
                     cursor={true}
-                    repeat={Infinity}
+                    
                     className="text-center text-xl sm:text-2xl p-4"
                     omitDeletionAnimation={true}
                 />
             ) : (
                 <p className="text-gray-500">Carregando...</p>
             )}
+            {showReloadButton && (
+            <button
+              onClick={() => window.location.reload()}
+              className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded-xl shadow-md mt-6"
+            >
+              Ver tudo de novo
+            </button>
+          )}
             </div>
 
         </>
